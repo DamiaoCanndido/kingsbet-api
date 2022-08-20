52c7b78306063ca8ce20dd346ab52c69bf04db61
DamiaoCanndido <tower_gamer@gmail.com>
2022-08-20 15:53:05 -0300

    Using auth dtos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,18 @@
 import { Body, Controller, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { SignInDTO, SignUpDTO } from "./dto";
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
 
     @Post('signup')
-    signup(@Body() dto: any){
-        console.log({
-            dto,
-        })
+    signup(@Body() dto: SignUpDTO){
         return this.authService.signup()
     }
 
     @Post('signin')
-    login(){
+    signin(@Body() dto: SignInDTO){
         return this.authService.signin()
     }
 }
