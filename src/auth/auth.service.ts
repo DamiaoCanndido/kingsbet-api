70ef860dbcf2b4b5959d2aadb4d8f0784a841275
DamiaoCanndido <tower_gamer@gmail.com>
2022-08-20 18:58:24 -0300

    Nestjs pipes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { SignUpDTO } from "./dto";
 
 @Injectable()
 export class AuthService {
 
     constructor(private prisma: PrismaService){}
     
-    signup(){
-        return {msg:'signup'}
+    signup(dto: SignUpDTO){
+        return {dto,}
     }
 
     signin(){
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,11 +1,33 @@
-export interface SignInDTO {
+import { Equals, equals, IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator"
+
+export class SignInDTO {
+    @IsEmail()
+    @IsNotEmpty()
     email: string
+
+    @MinLength(6)
+    @IsString()
+    @IsNotEmpty()
     password: string
 }
 
-export interface SignUpDTO {
+export class SignUpDTO {
+    @IsString()
+    @IsNotEmpty()
     name: string
+
+    @IsEmail()
+    @IsNotEmpty()
     email: string
+
+    @MinLength(6)
+    @IsString()
+    @IsNotEmpty()
     password: string
+
+    // Falta verificar a igualdade
+    @MinLength(6)
+    @IsString()
+    @IsNotEmpty()
     confirmPassword: string
 }
