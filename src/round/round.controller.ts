786eafd0b162270f91714f6f08768daab0a98923
DamiaoCanndido <tower_gamer@gmail.com>
2022-09-16 21:43:17 -0300

    Applying casl

--- src/round/round.controller.ts
+++ src/round/round.controller.ts
@@ -6,14 +6,24 @@ import {
   Put,
   Param,
   Delete,
+  UseFilters,
+  UseGuards,
 } from "@nestjs/common";
 import { RoundService } from "./round.service";
 import { CreateRoundDto, UpdateRoundDto } from "./dto";
+import { AllExceptionsFilter } from "../helpers/http-exception.filter";
+import { JwtGuard } from "../auth/guard";
+import { CaslAbilityGuard } from "../casl/guard";
+import { CheckCaslAbility } from "../casl/decorators";
+import { RoundAbility } from "../casl/decorators/round-abilities/round-abilities";
 
 @Controller("round")
+@UseFilters(AllExceptionsFilter)
 export class RoundController {
   constructor(private readonly roundService: RoundService) {}
 
+  @UseGuards(JwtGuard, CaslAbilityGuard)
+  @CheckCaslAbility(new RoundAbility().createRound())
   @Post()
   create(@Body() createRoundDto: CreateRoundDto) {
     return this.roundService.create(createRoundDto);
@@ -24,11 +34,15 @@ export class RoundController {
     return this.roundService.findByChamp(champId);
   }
 
+  @UseGuards(JwtGuard, CaslAbilityGuard)
+  @CheckCaslAbility(new RoundAbility().updateRound())
   @Put(":id")
   update(@Param("id") id: string, @Body() updateRoundDto: UpdateRoundDto) {
     return this.roundService.update(id, updateRoundDto);
   }
 
+  @UseGuards(JwtGuard, CaslAbilityGuard)
+  @CheckCaslAbility(new RoundAbility().deleteRound())
   @Delete(":id")
   remove(@Param("id") id: string) {
     return this.roundService.remove(id);
--- src/round/round.module.ts
+++ src/round/round.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { RoundService } from './round.service';
-import { RoundController } from './round.controller';
+import { Module } from "@nestjs/common";
+import { RoundService } from "./round.service";
+import { RoundController } from "./round.controller";
+import { CaslModule } from "../casl/casl.module";
 
 @Module({
   controllers: [RoundController],
-  providers: [RoundService]
+  providers: [RoundService],
+  imports: [CaslModule],
 })
 export class RoundModule {}
